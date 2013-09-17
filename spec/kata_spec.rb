fb198872aa57c4090e9c2716b6516786fd6c250f
Jones Agyemang <jones.k.agyemang@gmail.com>
2013-09-17 10:53:54 +0000

    Start afresh. Create new test cases and examine all scenarios

--- spec/kata_spec.rb
+++ spec/kata_spec.rb
@@ -1,18 +1,15 @@
-require './lib/kata_eight_extendable'
+File.expand_path( "../../lib", __FILE__ )
+
+require 'kata_eight_readable'
 
 describe Kata do 
-	before(:each) do 
-		@k = Kata.new(6, 'WordList.txt')
-	end
-
-	context "process dictionary" do 
-		it "should identify all dictionary words" do 
-			@k.dictionary_of_n_sized_words.should_not be_empty
+	describe "processing a dictionary" do 
+		context "dictionary does not exist" do 
+			pending ""
 		end
 
-		it "should successfully identify all words of length six" do 
-			k = Kata.new(6, 'WordList.txt')
-			k.dictionary_of_n_sized_words.select { |w| w.size != 6}.should be_empty
+		context "dictionary exists" do 
+			pending ""
 		end
 	end
 end
